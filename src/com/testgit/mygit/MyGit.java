d4b6f701bcc1f011c6e4dfb8c1ea00fa40f02522
qinghun.zl <qinghun.zl@alibaba-inc.com>
2015-12-14 14:49:05 +0800

    新建文件1

---
+++ src/com/testgit/mygit/MyGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.testgit.mygit;
+
+/**
+ * <p>Title: MyGit.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2007</p>
+ * <p>Company: Alipay</p>
+ * @author qinghun.zl
+ * @date 2015Äê12ÔÂ14ÈÕ
+ * @version 1.0
+ */
+public class MyGit {
+
+}
